0110997ba283b19bbb776e4b1098513108a5bc5f
hongya <hong7ya@gmail.com>
2025-02-27 01:56:41 +0900

    feat: 웹소켓을 얻고 크롬을 관리하기 위한 puppeteer 서버 세팅

---
+++ controllers/ws.js
@@ -0,0 +1,37 @@
+const { default: puppeteer } = require("puppeteer");
+
+module.exports.open = async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-infobars",
+      "--remote-debugging-port=9222",
+      "--remote-allow-origins=http://localhost:8000",
+      "about:blank"
+    ],
+    headless: false
+  });
+
+  const browserPID = browser.process().pid;
+
+  const response = await fetch("http://localhost:9222/json");
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const websocketTargets = await response.json();
+
+  const websocketTarget = websocketTargets.filter((websocketTarget) => websocketTarget.url === "about:blank" ? true : false)[0];
+  const wsParam = websocketTarget.webSocketDebuggerUrl.replace("ws://", '');
+
+  return res.status(200).json({
+    wsParam,
+    browserPID,
+  });
+}
+
+module.exports.close = async (req, res) => {
+  const browserPID = req.body.browserPID;
+  process.kill(browserPID);
+
+  return res.status(200).send('프로세스가 닫혔습니다.');
+}
---
+++ routes/ws.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const wsControllers = require('../controllers/ws');
+
+const router = express.Router();
+
+router.post('/open', wsControllers.open);
+
+router.post('/close', wsControllers.close);
+
+module.exports = router;
